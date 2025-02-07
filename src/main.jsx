400a295a9cfc94a4d8960ede4c609d051508c8ca
Your Name <peter895832@gmail.com>
2025-02-07 23:59:25 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ModeContextProvider } from './context/ModeContext.jsx'
-import Tester from './Tester.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
